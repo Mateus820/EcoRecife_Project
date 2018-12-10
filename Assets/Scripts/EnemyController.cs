9a3cf4ce6abc1d1e63c19336422279dc560df428
moesio-f <moesiof.dev@gmail.com>
2018-12-10 11:40:44 -0300

    Add Basic stage

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class EnemyController : MonoBehaviour {
+
+	[SerializeField] private int curHealth;
+	[SerializeField] private float speed;
+	[SerializeField] private bool direction;
+	[SerializeField] private Rigidbody rb;
+	
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+		int x;
+		if(direction) x = 1;
+		else x = -1;
+
+		rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, x * speed);
+	}
+
+	public void DecreaseLife(int damage){
+		if(curHealth <= 0){
+			Destroy(gameObject);
+			return;
+		}
+
+		curHealth -= damage;
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if(other.gameObject.tag == "DeathWall")
+			gameObject.SetActive(false);
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	[SerializeField] private float time;
+	[SerializeField] private Transform[] pointsToSpwan;
+
+	void Start () {
+		InvokeRepeating("Spawn", time, time);	
+	}
+
+	void Spawn(){
+
+		int random = Random.Range(0, pointsToSpwan.Length);
+
+		var obj = ObjectPooler.instance.GetPooledObject();
+		obj.transform.position = pointsToSpwan[random].position;
+		obj.SetActive(true);
+	}
+}
