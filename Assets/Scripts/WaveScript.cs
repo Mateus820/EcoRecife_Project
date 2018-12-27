2bcfa0ed0ff44d64591deb4ad45244796e6ad4da
Aluno <aluno@naverecife.local>
2018-12-27 13:21:19 -0300

    Testing Waves

--- Assets/Scripts/WaveScript.cs
+++ Assets/Scripts/WaveScript.cs
@@ -6,14 +6,17 @@ using UnityEngine.SceneManagement;
 public class WaveScript : MonoBehaviour
 {
     private Scene stageScene;
+    public int[] waveMonsterCount;
+    private int wave;   
+    public float spawnCooldown;
 
-    private int stageNumber;
 
-    public int[] waveMonsterCount;  
-    private int wave;
+    private string[] waveMonsters;
+    
     void Start()
     {
           waveMonsterCount = new int [5];      
+          waveMonsters = new string[5];
           stageScene = SceneManager.GetActiveScene();
          /*stage 1 = tutorial - dom
            stage 2 - dom
@@ -25,29 +28,76 @@ public class WaveScript : MonoBehaviour
            stage 8 - dom, jo, otto e riso
            stage 9 - dom, jo, otto e riso
            stage 10 - dom, jo, otto e riso*/
+           #region Stage 1 
+
+
+           /*obs : dps trocar o nome da scene "game" para stage 1 */
            if(stageScene.name == "Game")
-           {    
+           {  
+               wave = 0;  
                waveMonsterCount = new int [3]; 
-               stageNumber = 1;
                /*aqui indicará o numero de monstros de cada wave ,sempre q um monstro morrer,
                irá retirar um. se chegar a zero, vamos à prox wave*/
                waveMonsterCount[0] = 2;
                waveMonsterCount[1] = 2;
                waveMonsterCount[2] = 2;
+
+                waveMonsters[0] = "l,l";
+                waveMonsters[1] = "l,l";
+                waveMonsters[3] = "l,l";
+
+               
            }
+
+        #endregion Stage 1 
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Wave(string monstersToSpawn)
     {
-        
+        for (int i = 0; i < monstersToSpawn.Length; i++)
+        {
+            switch(monstersToSpawn[i])
+            {
+                case 'l':
+                //spawna o laranja;
+                break ;
+
+                case 'v':
+                //spawna o verde
+                break;
+
+                case 'p':
+                //spawna o preto
+                break;
+
+                case 'a':
+                //spawna o azul
+                break;
+
+                default:
+                //é apenas uma virgula , ignore
+                break;
+            }
+
+            yield return new WaitForSeconds(spawnCooldown);
+        }
     }
 
-    IEnumerator Stage1()
-    {
-
-
+    IEnumerator ManagingWaves()
+    {  
+        for(;;)
+        {
+             if(waveMonsterCount[wave] == 0)
+                {
+                 wave++;
+                 if(wave == 6)
+                 {
+                     print("fim da fase");
+                 }
+                Wave(waveMonsters[wave]);
+                }
+        yield return new WaitForSeconds(0.1f);
+        }
     }
-
 }
 
