b58187f0d51f9298c698038957c0a5f677b194ef
Aluno <aluno@naverecife.local>
2019-02-11 18:39:36 -0300

    PauseMenu_0.1

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseGame : MonoBehaviour
+{
+    
+    [SerializeField] private GameObject menuPause;
+    public bool isPaused;
+
+    void Start()
+    {
+        //menuPause.SetActive(false);
+        isPaused = false;
+    }
+
+    public void PauseBtnPress(){
+        if(!isPaused){
+            Pause();
+        }
+        else
+        {
+            Resume();
+        }
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0f;
+        //menuPause.SetActive(true);
+        isPaused = true;
+    }
+
+    void Resume(){
+        Time.timeScale = 1f;
+        //menuPause.SetActive(false);
+        isPaused = false;
+    }
+}
